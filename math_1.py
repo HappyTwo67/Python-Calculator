21eb49a931e7a68e9e317f99f18846599d51e10b
HappyTwo67 <bozaijibrael@gmail.com>
2025-10-04 13:28:23 -0500

    Add files via upload

---
+++ math_1.py
@@ -0,0 +1,27 @@
+print("This is a simple multiplication calculator")
+
+try:
+    num1 = int(input("First Number: "))
+    num2 = int(input("Second Number: "))
+
+    num3 = num1 * num2
+
+    if num1 > num2:
+        print(num1, "*", num2, "=", num3)
+    else:
+        print("The answer is", num3)
+
+    import pyperclip
+
+    copycheck = input("Copy result? (Y/N): ").upper()
+
+    if copycheck == "Y":
+        pyperclip.copy(str(num3))
+        print("Result copied to clipboard!")
+    elif copycheck == "N":
+        print("Ok.")
+    else:
+        print("That isn't Y or N")
+
+except ValueError:
+    print("That is not a number")
